59e7c2272215d788ff0c9820b01beaa4cac6003d
Rohin Sharma <rsharma1340@conestogac.on.ca>
2023-11-15 20:15:17 -0500

    Initial commit.

---
+++ Assets/Scripts/Stikky_Platform.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stikky_Platform : MonoBehaviour
+{
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Palyer")
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Palyer")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+    /**
+    private void OnTriggerEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Palyer")
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnTriggerExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Palyer")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }*/
+}
