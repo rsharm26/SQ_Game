bcb614825380b0cd2bd4d96410b9c4fb661cfea6
MinchulHwang1 <114952919+MinchulHwang1@users.noreply.github.com>
2023-12-13 19:10:53 -0500

    Add comment and delete some scripts which was made to test for the first time to make project

--- Assets/Scripts/Platform/CameraController.cs
+++ Assets/Scripts/Platform/CameraController.cs
@@ -1,3 +1,10 @@
+/* Filename: CameraController.cs
+ * Project: SQ Term Project PixelAndysAdventure
+ * By: Minchul Hwang
+ * Date: December 13, 2023
+ * Description: When first create a project, 
+ *              a function returns the camera to its original position when the character dies.
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
--- Assets/Scripts/Platform/Finish.cs
+++ Assets/Scripts/Platform/Finish.cs
@@ -1,3 +1,10 @@
+/* Filename: Finish.cs
+ * Project: SQ Term Project PixelAndysAdventure
+ * By: Minchul Hwang
+ * Date: December 13, 2023
+ * Description: When first create a project, 
+ *              a file that changes to the end screen when the game ends..
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +12,6 @@ using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
-    // Start is called before the first frame update
     private bool levelCompleted = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
